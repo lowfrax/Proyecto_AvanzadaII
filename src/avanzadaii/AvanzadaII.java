b4bf8712efe0618a3474e69bfe44255433b84e3f
kevinrodriguez100 <167909724+lowfrax@users.noreply.github.com>
2024-04-24 18:36:33 -0600

    i2

--- src/avanzadaii/AvanzadaII.java
+++ src/avanzadaii/AvanzadaII.java
@@ -14,7 +14,7 @@ public class AvanzadaII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("hola mundo 2");
     }
     
 }
