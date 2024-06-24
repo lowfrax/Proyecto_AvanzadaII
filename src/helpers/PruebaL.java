7502c997bfd95daae7da5e6489b2a1703fe8e363
laura <laura@DESKTOP-TCIVII7>
2024-06-23 18:39:55 -0600

    Prueba Laura

--- src/helpers/PruebaL.java
+++ src/helpers/PruebaL.java
@@ -1,9 +1,12 @@
-//A
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package helpers;
 
 /**
  *
- * @author laura
+ * @author ksalo
  */
 public class PruebaL {
     
